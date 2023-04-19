6c291e00517b7dc6fbd06b993240acb4faaa2816
Андрей Егоров <gremelya13@gmail.com>
2023-04-19 19:22:03 +0300

    правки(стрелочные функции, упрощение конструкции if else, исправление 3й задачи

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,9 +2,7 @@ function getArrayParams(...arr) {
 	min = Math.min(...arr);
 	max = Math.max(...arr);
 
-	sum = (arr.reduce(function(a, b) {
-		return a + b;
-	}) / arr.length);
+	sum = (arr.reduce((a, b) => a + b) / arr.length);
 	avg = Number(sum.toFixed(2));
 	return {
 		min: min,
@@ -13,91 +11,75 @@ function getArrayParams(...arr) {
 	};
 }
 
+
+
 function summElementsWorker(...arr) {
-	if (arr.length != 0) {
-		sum = arr.reduce(function(a, b) {
-			return a + b;
-		});
-		return sum;
-	} else {
-		return 0
+	if (arr.length === 0) {
+		return 0;
 	}
+	sum = arr.reduce((a, b) => a + b);
+	return sum;
+
 }
 
 function differenceMaxMinWorker(...arr) {
-	if (arr != '') {
-		min = Math.min(...arr);
-		max = Math.max(...arr);
-		return max - min;
-	} else {
+	if (arr.length === 0) {
 		return 0;
 	}
+	min = Math.min(...arr);
+	max = Math.max(...arr);
+	return max - min;
+
 
 }
 
 
 function differenceEvenOddWorker(...arr) {
-	if (arr != '') {
-		even = arr.filter(function(even) {
-			if (even % 2 == 0) {
-				return true
-			} else {
-				return false
-			}
-		});
-		sumEven = even.reduce(function(a, b) {
-			return a + b
-		});
-		odd = arr.filter(function(odd) {
-			if (odd % 2 != 0) {
-				return true
-			} else {
-				return false
-			}
-		})
-		sumOdd = odd.reduce(function(a, b) {
-			return a + b
-		})
-		return sumEven - sumOdd;
-	} else {
+	if (arr.length === 0) {
 		return 0;
 	}
+	even = arr.filter(function(even) {
+		return even % 2 == 0
+	});
+	sumEven = even.reduce(function(a, b) {
+		return a + b
+	});
+	odd = arr.filter(function(odd) {
+		return odd % 2 != 0
+	})
+	sumOdd = odd.reduce((a, b) => a + b)
+	return sumEven - sumOdd;
+
 }
 
 
 function averageEvenElementsWorker(...arr) {
-	if (arr != '') {
-		even = arr.filter(function(even) {
-			if (even % 2 == 0) {
-				return true
-			} else {
-				return false
-			}
-		})
-		sumEven = even.reduce(function(a, b) {
-			return a + b
-		});
-		return sumEven / even.length;
-	} else {
+	if (arr.length === 0) {
 		return 0;
 	}
+	even = arr.filter(function(even) {
+		return even % 2 == 0
+	})
+	sumEven = even.reduce(function(a, b) {
+		return a + b
+	});
+	return sumEven / even.length;
+
 }
 
 function makeWork(arrOfArr, func) {
-	if (arr != '') {
-		let maxWorkerResult = -Infinity;
-		let conconst = 0;
-		for (let i = 0; i < arr.length; i++) {
-			arrOfArr = arr[i]
-			conconst = func(...arrOfArr);
-			if (conconst >= maxWorkerResult) {
-				maxWorkerResult = conconst;
-			}
-		}
 
-		return maxWorkerResult;
-	} else {
-		return 0;
+	let maxWorkerResult = -Infinity;
+
+	for (let i = 0; i < arrOfArr.length; i++) {
+
+		const sum = func(...arrOfArr[i]);
+
+		if (sum > maxWorkerResult) {
+			maxWorkerResult = sum;
+		}
 	}
+	return maxWorkerResult;
+
 }
 
