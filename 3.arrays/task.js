c8363bc9c0df708338cf86fce42462639a3cd581
Андрей Егоров <gremelya13@gmail.com>
2023-04-23 14:52:56 +0300

    кодстайл соблюден

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,15 +2,13 @@ function compareArrays(a, b) {
 	return a.length === b.length && a.every((n, i) => n === b[i]);
 }
 
-	function getUsersNamesInAgeRange(user, gender) {
-		let filt = user.filter(user => user.gender === gender)
-		if (filt.length === 0) {
-			return 0
-		}
-		let map = filt.map(user => user.age)
-		let reduce = map.reduce((sum, current) => sum + current)
-
-		return reduce / map.length
+function getUsersNamesInAgeRange(user, gender) {
+	let filt = user.filter(user => user.gender === gender)
+	if (filt.length === 0) {
+		return 0
 	}
+	let map = filt.map(user => user.age)
+	let reduce = map.reduce((sum, current) => sum + current)
 
-  
+	return reduce / map.length
+}
