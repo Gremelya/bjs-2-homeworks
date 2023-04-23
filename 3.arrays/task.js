5e7f16469d66057d5f2f3034b229fc7cc758a3fa
Андрей Егоров <gremelya13@gmail.com>
2023-04-23 14:39:51 +0300

    2я задача не по канону

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(a, b) {
+	return a.length === b.length && a.every((n, i) => n === b[i]);
 }
+console.log(a.every((n, i) => n === b[i]))
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+	/
+	function getUsersNamesInAgeRange(user, gender) {
+		let filt = user.filter(user => user.gender === gender)
+		if (filt.length === 0) {
+			return 0
+		}
+		let map = filt.map(user => user.age)
+		let reduce = map.reduce((sum, current) => sum + current)
+
+		return reduce / map.length
+	}
