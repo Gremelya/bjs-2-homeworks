910f2c9c7ea1a44d53aab56592115c53f9357368
Андрей Егоров <gremelya13@gmail.com>
2023-05-02 13:53:31 +0300

    исправлено по разборному вебинару

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -20,7 +20,7 @@ Student.prototype.addMarks = function(...marks) {
 }
 
 Student.prototype.getAverage = function() {
-	if (this.marks.length === 0 || typeof this.marks === undefined) {
+	if (this.marks === undefined || this.marks.length === 0) {
 		return 0;
 	} else {
 		return (this.marks.reduce((previous, next) => previous + next, 0)) / this.marks.length;
@@ -33,7 +33,7 @@ Student.prototype.exclude = function(reason) {
 	this.excluded = reason;
 }
 
-let student1 = new Student("Василиса", "женский", 19);
+/*let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
@@ -44,4 +44,4 @@ let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
 student2.exclude('плохая учёба')
 console.log(student2)
-// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}*/
